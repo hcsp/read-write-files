6027a467fe1af48b1bfa1015e06de119811499df
Jchaokai <1428561219@qq.com>
2019-08-05 14:40:45 +0800

    使用多种方法读写文件

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -2,7 +2,14 @@ package com.github.hcsp.io;
 
 import org.apache.commons.io.IOUtils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -11,40 +18,27 @@ import java.util.List;
 public class FileAccessor {
     //  common io
     public static List<String> readFile1(File file)  {
-        FileInputStream fileInputStream = null;
         List<String> lines =null;
-        try {
-            fileInputStream = new FileInputStream(file);
+        try(FileInputStream fileInputStream = new FileInputStream(file);) {
             lines =  IOUtils.readLines(fileInputStream,"UTF-8");
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if (fileInputStream != null) {
-                try {
-                    fileInputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
         return lines;
     }
 
     //  最原始FileInputStream/FileOutputStream，一个字符一个字符的读取
-    public static List<String> readFile2(File file) throws IOException {
+    public static List<String> readFile2(File file) {
         List<String> list = new ArrayList();
-        FileInputStream fileInputStream = null;
-        try {
-            fileInputStream = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
+        try (FileInputStream fileInputStream = new FileInputStream(file);){
+            byte[] bytes = new byte[1024];
+            int len =-1;
+            while ((len = fileInputStream.read(bytes)) != -1) {
+                list.add(new String(bytes, 0, len));
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        byte[] bytes = new byte[1024];
-        int len =-1;
-        while ((len = fileInputStream.read(bytes)) != -1) {
-            list.add(new String(bytes, 0, len));
-        }
-        fileInputStream.close();
         return list;
 
     }
@@ -52,28 +46,17 @@ public class FileAccessor {
     //使用BufferReader/BufferedWriter，一行一行地读写文件
     public static List<String> readFile3(File file) {
         List<String> list = new ArrayList();
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader(file));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        String line = null;
-        try {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file));){
+            String line = null;
             line = bufferedReader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        while (line != null){
-            list.add(line);
-            try {
-                line = bufferedReader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
+            while (line != null){
+                list.add(line);
+                try {
+                    line = bufferedReader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        try {
-            bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -83,21 +66,14 @@ public class FileAccessor {
     // common io 写文件
     public static void writeLinesToFile1(List<String> lines, File file) {
         Iterator<String> iterator = lines.iterator();
-        FileOutputStream fileOutputStream = null;
-        try {
-            fileOutputStream = new FileOutputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        while (iterator.hasNext()){
-            try {
-                IOUtils.write(iterator.next(),fileOutputStream,"UTF-8");
-            } catch (IOException e) {
-                e.printStackTrace();
+        try(FileOutputStream fileOutputStream = new FileOutputStream(file);) {
+            while (iterator.hasNext()){
+                try {
+                    IOUtils.write(iterator.next(),fileOutputStream,"UTF-8");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        try {
-            fileOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -106,22 +82,15 @@ public class FileAccessor {
     // 最原始FileInputStream/FileOutputStream，一个字符一个字符的读取
     public static void writeLinesToFile2(List<String> lines, File file) {
         Iterator<String> iterator = lines.iterator();
-        FileOutputStream fileOutputStream = null;
-        try {
-            fileOutputStream = new FileOutputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        while (iterator.hasNext()){
-            byte[] bytes = iterator.next().getBytes();
-            try {
-                fileOutputStream.write(bytes);
-            } catch (IOException e) {
-                e.printStackTrace();
+        try(FileOutputStream fileOutputStream = new FileOutputStream(file);) {
+            while (iterator.hasNext()){
+                byte[] bytes = iterator.next().getBytes();
+                try {
+                    fileOutputStream.write(bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        try {
-            fileOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -129,22 +98,15 @@ public class FileAccessor {
 
     // 使用BufferReader/BufferedWriter，一行一行地读写文件
     public static void writeLinesToFile3(List<String> lines, File file) {
-        BufferedWriter bufferedWriter =null;
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter(file));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         Iterator<String> iterator = lines.iterator();
-        while (iterator.hasNext()){
-            try {
-                bufferedWriter.write(iterator.next());
-            } catch (IOException e) {
-                e.printStackTrace();
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));) {
+            while (iterator.hasNext()){
+                try {
+                    bufferedWriter.write(iterator.next());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        try {
-            bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
