1b84da6711a9e13a872370e64486d5f3c4106cc3
zorofun <choupeisi@gmail.com>
2020-03-01 18:03:19 +0800

    异常处理

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -9,97 +9,71 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {
+    public static List<String> readFile1(File file) throws IOException {
         List<String> list = new ArrayList<>();
-        try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-            while ((line =  bufferedReader.readLine())!=null){
-                list.add(line);
-            }
-            bufferedReader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            list.add(line);
         }
+        bufferedReader.close();
+
         return list;
     }
 
-   public static List<String> readFile2(File file) {
+    public static List<String> readFile2(File file) throws IOException {
         List<String> list = new ArrayList<>();
-       try {
-           list = FileUtils.readLines(file, Charset.defaultCharset());
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
-       return list;
-   }
+        list = FileUtils.readLines(file, Charset.defaultCharset());
+        return list;
+    }
 
-   public static List<String> readFile3(File file) {
-        List<String> list =new ArrayList<>();
-       try {
-           FileInputStream fileInputStream = new FileInputStream(file);
-           int readInt;
-           StringBuilder stringBuilder = new StringBuilder();
-           while ((readInt=fileInputStream.read())!=-1){
-               if (readInt==10||readInt==13){
-                   list.add(stringBuilder.toString());
-                   stringBuilder.delete(0,stringBuilder.length());
-               } else {
-                   stringBuilder.append((char) readInt);
-               }
-           }
-           fileInputStream.close();
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
-       return list;
-   }
+    public static List<String> readFile3(File file) throws IOException {
+        List<String> list = new ArrayList<>();
+        FileInputStream fileInputStream = new FileInputStream(file);
+        int readInt;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((readInt = fileInputStream.read()) != -1) {
+            if (readInt == 10 || readInt == 13) {
+                list.add(stringBuilder.toString());
+                stringBuilder.delete(0, stringBuilder.length());
+            } else {
+                stringBuilder.append((char) readInt);
+            }
+        }
+        fileInputStream.close();
+        return list;
+    }
 
-    public static void writeLinesToFile1(List<String> lines, File file) {
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
         FileWriter fileWriter = null;
-        try {
-            fileWriter = new FileWriter(file);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            for (String line:lines) {
-                    bufferedWriter.write(line);
-                    bufferedWriter.newLine();
+        fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        for (String line : lines) {
+            bufferedWriter.write(line);
+            bufferedWriter.newLine();
+        }
+        bufferedWriter.close();
+
+
+    }
+
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+        FileUtils.writeLines(file, lines);
+    }
+
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        for (String line : lines) {
+            for (int i = 0; i < line.length(); i++) {
+                fileOutputStream.write(line.charAt(i));
             }
-            bufferedWriter.flush();
-            bufferedWriter.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            fileOutputStream.write(10);
         }
-
-
+        fileOutputStream.close();
     }
 
-    public static void writeLinesToFile2(List<String> lines, File file) {
-        try {
-            FileUtils.writeLines(file,lines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void writeLinesToFile3(List<String> lines, File file) {
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            for (String line:lines) {
-                for (int i = 0; i < line.length(); i++) {
-                    fileOutputStream.write(line.charAt(i));
-                }
-                fileOutputStream.write(10);
-            }
-            fileOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
