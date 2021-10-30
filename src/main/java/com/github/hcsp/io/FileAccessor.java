2a671cfd37d86a5598e02e3785802d18dd082f41
MongieLee <le30n@outlook.com>
2021-10-30 19:41:11 +0800

    使用多种方法读写文件

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,23 +1,55 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
+    // 使用最原始的io读取
+    public static List<String> readFile1(File file) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            StringBuilder sb = new StringBuilder();
+            int c;
+            while ((c = fileInputStream.read()) != -1) {
+                sb.append((char) c);
+            }
+            return Arrays.asList(sb.toString().split("\n"));
+        }
+    }
 
-    public static List<String> readFile2(File file) {}
+    //  使用nio的api读取，java7+后新增
+    public static List<String> readFile2(File file) throws IOException {
+        return Files.readAllLines(file.toPath());
+    }
 
-    public static List<String> readFile3(File file) {}
+    //  使用IOUtils读取
+    public static List<String> readFile3(File file) throws IOException {
+        return IOUtils.readLines(new FileInputStream(file), "UTF-8");
+    }
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        byte[] bytes = String.join("\n", lines).getBytes();
+        for (byte aByte : bytes) {
+            fileOutputStream.write(aByte);
+        }
+        fileOutputStream.close();
+    }
 
-    public static void writeLinesToFile2(List<String> lines, File file) {}
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(String.join("\n",lines).getBytes());
+    }
 
-    public static void writeLinesToFile3(List<String> lines, File file) {}
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+        IOUtils.write(String.join("\n", lines), new FileOutputStream(file),"UTF-8");
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
