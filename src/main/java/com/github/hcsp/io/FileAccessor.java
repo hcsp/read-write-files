7049f3a0ea4b68cdd8f11ef450d167cc636bc430
3stonelei <1462070965@qq.com>
2022-06-07 11:38:56 +0800

    读写文件

--- src/main/java/com/github/hcsp/io/FileAccessor.java
+++ src/main/java/com/github/hcsp/io/FileAccessor.java
@@ -1,23 +1,84 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class FileAccessor {
-    public static List<String> readFile1(File file) {}
+    public static List<String> readFile1(File file) throws IOException {
 
-    public static List<String> readFile2(File file) {}
 
-    public static List<String> readFile3(File file) {}
+        return Files.readAllLines(file.toPath());
 
-    public static void writeLinesToFile1(List<String> lines, File file) {}
+    }
 
-    public static void writeLinesToFile2(List<String> lines, File file) {}
+    public static List<String> readFile2(File file) throws IOException {
+        List<String> readList=new ArrayList<>();
+        InputStream io =new FileInputStream(file);
+        byte[] buff=new byte[1024];
+        int len;
+        while ((len=io.read(buff))!=-1){
+            String s=new String(buff,0,len);
+            readList.add(s);
+        }
+        io.close();
 
-    public static void writeLinesToFile3(List<String> lines, File file) {}
+        return readList;
 
-    public static void main(String[] args) {
+    }
+
+    public static List<String> readFile3(File file) throws IOException {
+
+        List<String> readList=new ArrayList<>();
+        //字符流
+        //br缓冲流
+        BufferedReader br=new BufferedReader(new FileReader(file));
+        //存取读取字符的字符数组
+        char[] cbuf=new char[1024];
+        int len;
+        while ((len=br.read(cbuf))!=-1){
+            String s = String.valueOf(cbuf, 0, len);
+            readList.add(s);
+        }
+        br.close();
+
+        return readList;
+    }
+
+    public static void writeLinesToFile1(List<String> lines, File file) throws IOException {
+        Files.write(file.toPath(),lines, Charset.defaultCharset());
+    }
+
+    public static void writeLinesToFile2(List<String> lines, File file) throws IOException {
+        OutputStream os=new FileOutputStream(file);
+        //Iterator迭代器，每次调用都会得到一个新的迭代器对象
+        Iterator<String> iterator = lines.iterator();
+        //hasNext 判断是否有下一个元素
+        while (iterator.hasNext()){
+            os.write(iterator.next().getBytes());
+        }
+        os.close();
+
+
+    }
+
+    public static void writeLinesToFile3(List<String> lines, File file) throws IOException {
+        BufferedWriter bw=new BufferedWriter(new FileWriter(file));
+        Iterator<String> iterator = lines.iterator();
+        while (iterator.hasNext()){
+            bw.write(iterator.next());
+
+        }
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         File testFile = new File(projectDir, "target/test.txt");
         List<String> lines = Arrays.asList("AAA", "BBB", "CCC");
